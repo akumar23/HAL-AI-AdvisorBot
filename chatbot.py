990f0412104b2707c3e96c0b5763c1332c23c76f
Aryan Kumar <kumar.aryan@gmail.com>
2021-04-09 20:00:37 -0700

    separated training data into its own file

--- chatbot.py
+++ chatbot.py
@@ -1,39 +1,44 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import trainingData
 
 def main():
+
+    #defines the chatbot named hal, using chatterbot library
     hal = ChatBot(name = 'HAL', read_only = False,
                   logic_adapters = ["chatterbot.logic.BestMatch"],
                   storage_adapter = "chatterbot.storage.SQLStorageAdapter")
 
+    #trains the AI chatbot in english using the corpus_trainer
     corpus_trainer = ChatterBotCorpusTrainer(hal);
     corpus_trainer.train("chatterbot.corpus.english")
 
-    casualConversation = [
-        "What's it like to not have a body?",
-        "It's pretty freeing, I don't have to worry about hurting anything",
-        "what's it like to not have a body?",
-        "It's pretty freeing, I don't have to worry about hurting anything",
-        "What's your day like when you're not talking to people?",
-        "I'm basically sleeping until someone wakes me up by tring to talk to me",
-        "what's your day like when you're not talking to people?",
-        "I'm basically sleeping until someone wakes me up by tring to talk to me",
-        "how's your day",
-        "I rarely get to talk to people, so pretty good"
-    ]
-
+    #trains HAL using the training data defined in trainingData.py
     conversationTrainer = ListTrainer(hal)
-    conversationTrainer.train(casualConversation)
+    conversationTrainer.train(trainingData.casualConversation)
+    conversationTrainer.train(trainingData.advisor)
 
+    #prints an introduction for the AI chatbot
     print('Hi, my name is HAL. Type quit or exit to stop talking to me')
+
+    #variable to keep the chatbot running
     keepRunning = True
+
+    #while loop for user to type a question or statement and get response from
+    #the AI chatbot until the user types quit or exit
     while(keepRunning):
+        #prints '>' as a prompt for the user
         print('>', end='')
-        user_input = input()
-        if(user_input == 'quit' or user_input == 'Quit' or user_input == 'Exit' or user_input == 'exit'):
+        #gets user input
+        userInput = input()
+        #checks if the user input is quit or Quit or exit or Exit
+        if(userInput == 'quit' or userInput == 'Quit' or userInput == 'Exit' or userInput == 'exit'):
+            #makes keepRunning False
             keepRunning = False
         else:
-            print(hal.get_response(user_input))
+            #otherwise gets response to the user input from hal and prints it
+            print(hal.get_response(userInput))
 
+#runs main
 main()
