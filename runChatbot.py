30f0e9a93981c694e3c6aeab45719f10e3085abe
Aryan Kumar <kumar.aryan@gmail.com>
2021-11-28 02:58:12 -0800

    added more questions to the training data

--- runChatbot.py
+++ runChatbot.py
@@ -29,8 +29,8 @@ conversationTrainer.train(trainingData.gpaToTransfer)
 
 correctTypos = SpellChecker()
 
-tag_list = ['cs 149', 'ise 164', 'cs 146', 'cmpe 131', 'cmpe 120', 'cmpe 102', 'cmpe 133', 'cmpe 148', 'cmpe 165', 'cmpe 172', 'cmpe 187', 'cmpe 195a', 'cmpe 195b', 'engr 195a', 'engr 195b', 'engr 195', 'cmpe 195', 'cmpe195', 'engr195', 'cs 151', 'cs 157a', 'cs 166', 'cs149', 'ise164', 'cs146', 'cmpe131', 'cmpe120', 'cmpe102', 'cmpe133', 'cmpe148', 'cmpe165', 'cmpe172', 'cmpe187', 'cmpe195a', 'cmpe195b', 'engr195a', 'engr195b', 'engr195', 'cmpe195', 'cs151', 'cs157a', 'cs166', 'how many units should i take']
-prereq = ['prerequisite', 'prereq', 'prerequisites', 'prereqs']
+tag_list = ['cs 149', 'ise 164', 'cs 146', 'cmpe 131', 'cmpe 120', 'cmpe 102', 'cmpe 133', 'cmpe 148', 'cmpe 165', 'cmpe 172', 'cmpe 187', 'cmpe 195a', 'cmpe 195b', 'engr 195a', 'engr 195b', 'engr 195', 'cmpe 195', 'cmpe195', 'engr195', 'cs 151', 'cs 157a', 'cs 166', 'cs149', 'ise164', 'cs146', 'cmpe131', 'cmpe120', 'cmpe102', 'cmpe133', 'cmpe148', 'cmpe165', 'cmpe172', 'cmpe187', 'cmpe195a', 'cmpe195b', 'engr195a', 'engr195b', 'engr195', 'cmpe195', 'cs151', 'cs157a', 'cs166', 'how many units should i take', 'cmpe 137', 'cmpe137', 'cmpe 139', 'cmpe139', 'cmpe 152', 'cmpe152', 'cmpe 185', 'cmpe185', 'cmpe 181', 'cmpe181', 'cmpe 182', 'cmpe182', 'cmpe 183', 'cmpe183', 'cmpe 185', 'cmpe185', 'cmpe 188', 'cmpe188', 'cmpe 189', 'cmpe189', 'cs 116a', 'cs116a', 'cs 134', 'cs134', 'cs 152', 'cs152']
+prereq = ['prerequisite', 'prereq', 'prerequisites', 'prereqs', 'take before', 'what class do i need to']
 
 @app.route('/')
 def index():
@@ -43,15 +43,20 @@ def getResponse():
     userMessage = correctTypos.correction(userMessage)
     tag = [s for s in tag_list if(s in userMessage)]
     hasPrereq = [s for s in prereq if(s in userMessage)]
-    if(bool(tag)):
+    if(bool(tag) and bool(hasPrereq)):
         selected_intent = next((i for i in trainingData.overallPrereq if i['tag'] == tag[0]), None)
         possibleResponses = selected_intent['responses']
-        response = possibleResponses[random.randint(0, len(possibleResponses)-1)]
+        response = possibleResponses[0]
         return str(response)
     elif(bool(hasPrereq) and not(bool(tag))):
         return "sorry i don't know the prerequiste for that. you can check using the course catalog here: https://catalog.sjsu.edu/content.php?catoid=12&navoid=4145"
+    elif(not(bool(hasPrereq)) and bool(tag)):
+        selected_intent = next((i for i in trainingData.overallPrereq if i['tag'] == tag[0]), None)
+        possibleResponses = selected_intent['responses']
+        response = possibleResponses[1]
+        return str(response)
     else:
-        return str(hal.get_response(userMessage))
+        return str(hal.get_response(userMessage)) + " If you don't like my answer, you can reword the question and ask it again or as me who your advisor is to get more indepth answers. I'm ready for more advising related questions."
 
 if __name__ == '__main__':
     app.run(debug=True)
